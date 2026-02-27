1a7d42d7c1127f625b0ab1048eb26635832bca1f
Seungwon-Choi <bestwondev@gmail.com>
2026-02-27 18:09:44 +0900

    chore: Spring boot 프로젝트 추가

---
+++ src/main/java/com/nextdaydelivery/DeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nextdaydelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryBackendApplication.class, args);
+    }
+
+}
