d1ce641db29ae2549b2b6e7b847274357832e883
woongcheol Na <32201345@dankook.ac.kr>
2026-03-03 15:35:17 +0900

    chore : 엔티티 기본 틀 작성 _ 중복 제거, 개행문자 삽입

--- src/main/java/com/nextdaydelivery/DeliveryBackendApplication.java
+++ src/main/java/com/nextdaydelivery/DeliveryBackendApplication.java
@@ -2,11 +2,9 @@ package com.nextdaydelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class DeliveryBackendApplication {
+public class DeliveryBackendApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryBackendApplication.class, args);
