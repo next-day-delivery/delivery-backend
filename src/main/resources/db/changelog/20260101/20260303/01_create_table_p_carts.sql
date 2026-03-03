aa0beabb73a354581c8891a676086119d6a65068
t2025-m0135 <junwon2631@gmail.com>
2026-03-03 23:12:20 +0900

    외래키 제약조건 수정

--- src/main/resources/db/changelog/20260101/20260303/01_create_table_p_carts.sql
+++ src/main/resources/db/changelog/20260101/20260303/01_create_table_p_carts.sql
@@ -6,15 +6,20 @@ CREATE TABLE p_cart
     cart_id      UUID PRIMARY KEY,
     user_id      BIGINT      NOT NULL,
     store_id     UUID        NOT NULL,
-    cart_status  VARCHAR(20) NOT NULL DEFAULT 'ACTIVE'
+    cart_status  VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
 
-        CONSTRAINT ck_cart_status CHECK (cart_status IN ('ACTIVE', 'INACTIVE', 'COMPLETED'))
+    CONSTRAINT ck_cart_status
+        CHECK (cart_status IN ('ACTIVE', 'INACTIVE', 'COMPLETED'))
 );
 
 -- rollback DROP TABLE p_cart;
 
-
 -- changeset Junwon-Kim:02-create-p-cart-indexes
-CREATE INDEX idx_cart_user_status ON p_cart (user_id, cart_status);
+CREATE INDEX idx_cart_user_status
+    ON p_cart (user_id, cart_status);
+
+CREATE INDEX idx_cart_store_id
+    ON p_cart (store_id);
 
 -- rollback DROP INDEX idx_cart_user_status;
+-- rollback DROP INDEX idx_cart_store_id;
--- src/main/resources/db/changelog/20260101/20260303/02_create_table_p_cartItems.sql
+++ src/main/resources/db/changelog/20260101/20260303/02_create_table_p_cartItems.sql
@@ -1,20 +1,23 @@
 -- liquibase formatted sql
 
--- changeset Junwon-Kim:01-create-p-cart-item-table
+-- changeset Junwon-Kim:03-create-p-cart-item-table
 CREATE TABLE p_cart_item
 (
     cart_id    UUID   NOT NULL,
     product_id UUID   NOT NULL,
     quantity   BIGINT NOT NULL DEFAULT 1,
 
-    CONSTRAINT pk_cart_item PRIMARY KEY (cart_id, product_id),
-    CONSTRAINT ck_cart_item_quantity CHECK (quantity > 0)
+    CONSTRAINT pk_cart_item
+        PRIMARY KEY (cart_id, product_id),
+
+    CONSTRAINT ck_cart_item_quantity
+        CHECK (quantity > 0)
 );
 
 -- rollback DROP TABLE p_cart_item;
 
-
--- changeset Junwon-Kim:02-create-p-cart-item-indexes
-CREATE INDEX idx_cart_item_product_id ON p_cart_item (product_id);
+-- changeset Junwon-Kim:04-create-p-cart-item-indexes
+CREATE INDEX idx_cart_item_product_id
+    ON p_cart_item (product_id);
 
 -- rollback DROP INDEX idx_cart_item_product_id;
