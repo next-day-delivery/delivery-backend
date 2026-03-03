b4c18f592f56516e31c6346681fa64c0b8caf735
t2025-m0135 <junwon2631@gmail.com>
2026-03-03 21:43:25 +0900

    chore : cart,cartItem DDL 작성

---
+++ src/main/resources/db/changelog/20260101/20260303/01_create_table_p_carts.sql
@@ -0,0 +1,20 @@
+-- liquibase formatted sql
+
+-- changeset Junwon-Kim:01-create-p-cart-table
+CREATE TABLE p_cart
+(
+    cart_id      UUID PRIMARY KEY,
+    user_id      BIGINT      NOT NULL,
+    store_id     UUID        NOT NULL,
+    cart_status  VARCHAR(20) NOT NULL DEFAULT 'ACTIVE'
+
+        CONSTRAINT ck_cart_status CHECK (cart_status IN ('ACTIVE', 'INACTIVE', 'COMPLETED'))
+);
+
+-- rollback DROP TABLE p_cart;
+
+
+-- changeset Junwon-Kim:02-create-p-cart-indexes
+CREATE INDEX idx_cart_user_status ON p_cart (user_id, cart_status);
+
+-- rollback DROP INDEX idx_cart_user_status;
