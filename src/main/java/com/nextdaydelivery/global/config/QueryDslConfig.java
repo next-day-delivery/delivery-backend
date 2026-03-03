1cd3d3b7e0998c765aa870bed70707234f67fc44
Seungwon-Choi <bestwondev@gmail.com>
2026-03-03 15:27:20 +0900

    fix: ConditionalOnBean 어노테이션 추가

--- src/main/java/com/nextdaydelivery/global/config/QueryDslConfig.java
+++ src/main/java/com/nextdaydelivery/global/config/QueryDslConfig.java
@@ -2,11 +2,14 @@ package com.nextdaydelivery.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnBean(EntityManagerFactory.class)
 public class QueryDslConfig {
 
     @PersistenceContext
