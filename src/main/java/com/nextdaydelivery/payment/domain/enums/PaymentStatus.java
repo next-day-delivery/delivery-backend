f8fc23503f907a9b2751b2b243faeb327e9c1c3e
Sehi55 <sehi1028@naver.com>
2026-03-04 14:42:02 +0900

    fix: 결제 상태 ENUM 수정

--- src/main/java/com/nextdaydelivery/payment/domain/enums/PaymentStatus.java
+++ src/main/java/com/nextdaydelivery/payment/domain/enums/PaymentStatus.java
@@ -4,7 +4,8 @@ package com.nextdaydelivery.payment.domain.enums;
  * 결제 상태를 위한 ENUM
  */
 public enum PaymentStatus {
-    CANCEL,
-    COMPLETE,
+    PENDING,
+    CANCELED,
+    COMPLETED,
     FAILED
 }
