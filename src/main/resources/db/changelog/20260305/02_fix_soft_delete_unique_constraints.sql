2d2c4e824b36be1d62bb311a6606a15e8c9881b7
Seungwon-Choi <bestwondev@gmail.com>
2026-03-05 15:11:39 +0900

    fix: user 테이블 제약조건 수정

---
+++ src/main/resources/db/changelog/20260305/02_fix_soft_delete_unique_constraints.sql
@@ -0,0 +1,18 @@
+-- liquibase formatted sql
+
+-- changeset Seungwon-Choi:18-fix-soft-delete-unique-constraints
+ALTER TABLE p_user
+    DROP CONSTRAINT uk_users_username;
+ALTER TABLE p_user
+    DROP CONSTRAINT uk_users_email;
+
+DROP INDEX idx_users_email_active;
+DROP INDEX idx_users_username_active;
+
+CREATE UNIQUE INDEX uk_users_username_active ON p_user (username) WHERE deleted_at IS NULL;
+CREATE UNIQUE INDEX uk_users_email_active ON p_user (email) WHERE deleted_at IS NULL;
+
+-- rollback DROP INDEX uk_users_username_active;
+-- rollback DROP INDEX uk_users_email_active;
+-- rollback ALTER TABLE p_user ADD CONSTRAINT uk_users_email UNIQUE (email);
+-- rollback ALTER TABLE p_user ADD CONSTRAINT uk_users_username UNIQUE (username);
