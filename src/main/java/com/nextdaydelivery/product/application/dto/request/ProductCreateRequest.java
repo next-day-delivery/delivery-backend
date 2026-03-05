05994ce023a79b97542d4628e0f93a9a3ce4213f
gnoesnooj <306yyy@naver.com>
2026-03-05 14:09:59 +0900

    refactor: dto 입력 값 검증 어노테이션 추가

--- src/main/java/com/nextdaydelivery/product/application/dto/request/ProductCreateRequest.java
+++ src/main/java/com/nextdaydelivery/product/application/dto/request/ProductCreateRequest.java
@@ -1,12 +1,15 @@
 package com.nextdaydelivery.product.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.util.UUID;
 
 public record ProductCreateRequest(
-        UUID storeId,
-        String productName,
+        @NotNull UUID storeId,
+        @NotBlank String productName,
         String productDetail,
-        int price,
+        @Positive int price,
         boolean useAi
 ) {
 }
--- src/main/java/com/nextdaydelivery/product/application/dto/request/ProductUpdateRequest.java
+++ src/main/java/com/nextdaydelivery/product/application/dto/request/ProductUpdateRequest.java
@@ -1,9 +1,10 @@
 package com.nextdaydelivery.product.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record ProductUpdateRequest(
-        UUID id,
+        @NotNull UUID id,
         String productDetail,
         String productName,
         Integer price
