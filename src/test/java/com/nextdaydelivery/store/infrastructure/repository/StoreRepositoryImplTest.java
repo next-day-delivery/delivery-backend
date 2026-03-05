9274c35c96d9d897e3f45b7418004f7d4e010452
RunSBS <dacapo629@naver.com>
2026-03-05 14:51:37 +0900

    test: Store 리포지토리 테스트 파일 준비

---
+++ src/test/java/com/nextdaydelivery/store/infrastructure/repository/StoreRepositoryImplTest.java
@@ -0,0 +1,22 @@
+package com.nextdaydelivery.store.infrastructure.repository;
+
+import org.junit.jupiter.api.Test;
+
+class StoreRepositoryImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
