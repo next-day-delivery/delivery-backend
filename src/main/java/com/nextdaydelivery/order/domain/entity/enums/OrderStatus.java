35676340f05f814e77e4938c4a7c7442c5237510
woongcheol Na <32201345@dankook.ac.kr>
2026-03-06 17:04:07 +0900

    필요 없는 주석 제거

--- src/main/java/com/nextdaydelivery/order/domain/entity/enums/OrderStatus.java
+++ src/main/java/com/nextdaydelivery/order/domain/entity/enums/OrderStatus.java
@@ -1,8 +1,5 @@
 package com.nextdaydelivery.order.domain.entity.enums;
 
-/**
- * 주문 상태 관리를 위한 ENUM -> 추후 파일 분리
- */
 public enum OrderStatus {
     ORDER_REQUESTED,  // 주문 요청
     ORDER_REJECTED,   // 주문 거절
