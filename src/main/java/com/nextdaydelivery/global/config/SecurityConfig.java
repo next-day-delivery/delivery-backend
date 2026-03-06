e717fc34cfa5654fa640af685645d5aa78419bc7
Seungwon-Choi <bestwondev@gmail.com>
2026-03-06 12:28:43 +0900

    refactor: PUBLIC_URL 관리 방식 수정

--- src/main/java/com/nextdaydelivery/global/config/SecurityConfig.java
+++ src/main/java/com/nextdaydelivery/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nextdaydelivery.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,14 +17,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private static final String[] PUBLIC_URLS = {
-            "/",
+    private static final String[] PUBLIC_POST_URLS = {
             "/api/users",
-            "/api/auth/sign-in",
+            "/api/auth/sign-in"
     };
 
-    private static final String[] API_URLS = {
-            // TODO API URL 추가
+    private static final String[] PUBLIC_GET_URLS = {
+            // TODO 공개 GET API 추가
     };
 
     @Bean
@@ -35,8 +35,9 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_URLS).permitAll()
-                        .requestMatchers(API_URLS).authenticated()
+                        .requestMatchers(HttpMethod.POST, PUBLIC_POST_URLS).permitAll()
+                        .requestMatchers(HttpMethod.GET, PUBLIC_GET_URLS).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/actuator/**").hasRole("MASTER")
                         .anyRequest().authenticated()
                 )
