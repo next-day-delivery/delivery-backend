d5d1926a1ca82520707659b905878c9c712a250f
Seungwon-Choi <bestwondev@gmail.com>
2026-03-06 11:46:49 +0900

    feat: Spring security 설정 추가

---
+++ src/main/java/com/nextdaydelivery/global/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.nextdaydelivery.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private static final String[] PUBLIC_URLS = {
+            "/",
+            "/api/users",
+            "/api/auth/sign-in",
+    };
+
+    private static final String[] API_URLS = {
+            // TODO API URL 추가
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_URLS).permitAll()
+                        .requestMatchers(API_URLS).authenticated()
+                        .requestMatchers("/actuator/**").hasRole("MASTER")
+                        .anyRequest().authenticated()
+                )
+        ;
+
+        return http.build();
+    }
+}
