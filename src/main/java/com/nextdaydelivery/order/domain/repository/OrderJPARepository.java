5b6bd3d8001bb7e1824bb27f9ad1a111b5481a09
Sehi55 <sehi1028@naver.com>
2026-03-06 11:57:28 +0900

    fix: 주문 sql 쿼리 수정

--- src/main/java/com/nextdaydelivery/order/domain/repository/OrderJPARepository.java
+++ src/main/java/com/nextdaydelivery/order/domain/repository/OrderJPARepository.java
@@ -1,15 +1,19 @@
 package com.nextdaydelivery.order.domain.repository;
 
 import com.nextdaydelivery.order.domain.entity.Order;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface OrderJPARepository extends JpaRepository<Order, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)    // 사용자,매니저랑 동시에 상태 변경을 진행할 경우 대비
     @Query("Select o from Order o " +
-            "join Store s " +
+            "join o.store s " +
             "where o.orderId = :orderId and s.user.userId = :userId"
     )
     Optional<Order> findByIdAndOwnerId(@Param("orderId") UUID orderId, @Param("userId") Long userId);
