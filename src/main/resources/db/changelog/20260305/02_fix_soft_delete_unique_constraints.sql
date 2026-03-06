9599a3073de5b5e787b4d8874af15a35e6695715
Seungwon-Choi <bestwondev@gmail.com>
2026-03-06 12:13:16 +0900

    fix: 유니크 제약 조건 수정 changeset 수정

--- src/main/resources/db/changelog/20260305/02_fix_soft_delete_unique_constraints.sql
+++ src/main/resources/db/changelog/20260305/02_fix_soft_delete_unique_constraints.sql
@@ -2,12 +2,12 @@
 
 -- changeset Seungwon-Choi:18-fix-soft-delete-unique-constraints
 ALTER TABLE p_user
-    DROP CONSTRAINT uk_users_username;
+    DROP CONSTRAINT IF EXISTS uk_users_username;
 ALTER TABLE p_user
-    DROP CONSTRAINT uk_users_email;
+    DROP CONSTRAINT IF EXISTS uk_users_email;
 
-DROP INDEX idx_users_email_active;
-DROP INDEX idx_users_username_active;
+DROP INDEX IF EXISTS idx_users_email_active;
+DROP INDEX IF EXISTS idx_users_username_active;
 
 CREATE UNIQUE INDEX uk_users_username_active ON p_user (username) WHERE deleted_at IS NULL;
 CREATE UNIQUE INDEX uk_users_email_active ON p_user (email) WHERE deleted_at IS NULL;
@@ -16,3 +16,5 @@ CREATE UNIQUE INDEX uk_users_email_active ON p_user (email) WHERE deleted_at IS 
 -- rollback DROP INDEX uk_users_email_active;
 -- rollback ALTER TABLE p_user ADD CONSTRAINT uk_users_email UNIQUE (email);
 -- rollback ALTER TABLE p_user ADD CONSTRAINT uk_users_username UNIQUE (username);
+-- rollback CREATE INDEX idx_users_email_active ON p_user (email) WHERE deleted_at IS NULL;
+-- rollback CREATE INDEX idx_users_username_active ON p_user (username) WHERE deleted_at IS NULL;
