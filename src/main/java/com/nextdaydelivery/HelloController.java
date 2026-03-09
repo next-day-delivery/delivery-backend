f0efcfbf3851e4a8c23c8c63826ce71c57dd209d
t2025-m0135 <junwon2631@gmail.com>
2026-03-09 14:53:36 +0900

    test6

--- src/main/java/com/nextdaydelivery/HelloController.java
+++ src/main/java/com/nextdaydelivery/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/health")
     public String hello() {
-        return "Congratulation EC2 test success! version2";
+        return "Congratulation EC2 test success! version3";
     }
 }
 
