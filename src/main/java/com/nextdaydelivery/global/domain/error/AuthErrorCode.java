2b13c6b24cfdae2bef828277cd0cd374b1fe5e46
Seungwon-Choi <bestwondev@gmail.com>
2026-03-09 12:09:42 +0900

    feat: JwtValidator 구현

--- src/main/java/com/nextdaydelivery/global/domain/error/AuthErrorCode.java
+++ src/main/java/com/nextdaydelivery/global/domain/error/AuthErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthErrorCode implements ErrorCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, "A001", "접근 권한이 없습니다."),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "A002", "유효하지 않은 자격 증명입니다.");
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "A002", "유효하지 않은 자격 증명입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A003", "만료된 토큰입니다;"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "유효하지 않은 토큰입니다."),
 
     private final HttpStatus status;
     private final String code;
