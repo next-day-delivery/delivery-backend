b109e14ea4857104df59625dd74e7d02b3abf264
RunSBS <dacapo629@naver.com>
2026-03-09 14:19:48 +0900

    fix: SecurityAutoConfiguration 포함

--- src/main/java/com/nextdaydelivery/DeliveryBackendApplication.java
+++ src/main/java/com/nextdaydelivery/DeliveryBackendApplication.java
@@ -2,9 +2,8 @@ package com.nextdaydelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class DeliveryBackendApplication {
 
     public static void main(String[] args) {
