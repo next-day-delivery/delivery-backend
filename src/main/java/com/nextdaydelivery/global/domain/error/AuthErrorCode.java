0db8d5c481fa92cd9ef8bbbecf0ad38953d2c247
Seungwon-Choi <bestwondev@gmail.com>
2026-03-09 12:10:27 +0900

    feat: AuthUserArgumentResolver 구현

--- src/main/java/com/nextdaydelivery/global/domain/error/AuthErrorCode.java
+++ src/main/java/com/nextdaydelivery/global/domain/error/AuthErrorCode.java
@@ -11,6 +11,7 @@ public enum AuthErrorCode implements ErrorCode {
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "A002", "유효하지 않은 자격 증명입니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A003", "만료된 토큰입니다;"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "유효하지 않은 토큰입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A005", "인증되지 않은 사용자입니다.");
 
     private final HttpStatus status;
     private final String code;
---
+++ src/main/java/com/nextdaydelivery/global/security/dto/AuthUser.java
@@ -0,0 +1,11 @@
+package com.nextdaydelivery.global.security.dto;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthUser {
+}
