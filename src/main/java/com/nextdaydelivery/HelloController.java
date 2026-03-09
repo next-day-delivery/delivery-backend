7b0220d092b518e606631ede29aa45447be4f554
t2025-m0135 <junwon2631@gmail.com>
2026-03-09 15:53:45 +0900

    test9: main branch deploy test

--- src/main/java/com/nextdaydelivery/HelloController.java
+++ src/main/java/com/nextdaydelivery/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/health")
     public String hello() {
-        return "Congratulation EC2 test success! version3";
+        return "Congratulation EC2 test success! for main branch";
     }
 }
 
