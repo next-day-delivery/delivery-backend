f0a3f6f44944b45aa1bec0972a18fd8f6b1ade96
RunSBS <dacapo629@naver.com>
2026-03-09 12:06:08 +0900

    refactor: QueryDslConfig

--- src/main/java/com/nextdaydelivery/global/config/QueryDslConfig.java
+++ src/main/java/com/nextdaydelivery/global/config/QueryDslConfig.java
@@ -2,19 +2,14 @@ package com.nextdaydelivery.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ConditionalOnBean(EntityManagerFactory.class)
 public class QueryDslConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
         return new JPAQueryFactory(entityManager);
     }
 }
