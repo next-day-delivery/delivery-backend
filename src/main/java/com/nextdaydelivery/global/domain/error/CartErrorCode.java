30d595e335f65a9e6735956fc80db7a2751aa753
t2025-m0135 <junwon2631@gmail.com>
2026-03-09 10:36:27 +0900

    [Refactoring] CommonResponse 적용

---
+++ src/main/java/com/nextdaydelivery/global/domain/error/CartErrorCode.java
@@ -0,0 +1,20 @@
+package com.nextdaydelivery.global.domain.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum CartErrorCode implements ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "C001", "회원을 찾을 수 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "C002", "상품을 찾을 수 없습니다."),
+    ACTIVE_CART_NOT_FOUND(HttpStatus.NOT_FOUND, "C003", "활성화된 장바구니가 없습니다."),
+    CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "C004", "장바구니 품목을 찾을 수 없습니다."),
+    INVALID_CART_ITEM_QUANTITY(HttpStatus.BAD_REQUEST, "C005", "장바구니 수량은 0보다 커야 합니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
