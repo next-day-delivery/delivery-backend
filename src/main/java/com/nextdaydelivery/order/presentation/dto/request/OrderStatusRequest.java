62e635b650ae9d8d02dcb17ddf27f8390d717ede
woongcheol Na <32201345@dankook.ac.kr>
2026-03-10 15:19:42 +0900

    fix : 패키지 경로 수정

--- src/main/java/com/nextdaydelivery/order/presentation/dto/request/OrderStatusRequest.java
+++ src/main/java/com/nextdaydelivery/order/presentation/dto/request/OrderStatusRequest.java
@@ -1,9 +1,9 @@
 package com.nextdaydelivery.order.presentation.dto.request;
 
-import com.nextdaydelivery.order.domain.enums.OrderStatus;
+import com.nextdaydelivery.order.domain.entity.enums.OrderStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderStatusRequest(
-        @NotNull OrderStatus orderStatus
+    @NotNull OrderStatus orderStatus
 ) {
 }
