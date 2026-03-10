9a48bec4b4f5df37953e18294b1ebf8496332bdb
gnoesnooj <306yyy@naver.com>
2026-03-10 10:28:34 +0900

    refactor: ProductRepository 구조 변경에 따른 리팩토링

--- src/main/java/com/nextdaydelivery/product/domain/repository/ProductRepository.java
+++ src/main/java/com/nextdaydelivery/product/domain/repository/ProductRepository.java
@@ -1,8 +1,25 @@
 package com.nextdaydelivery.product.domain.repository;
 
 import com.nextdaydelivery.product.domain.entity.Product;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
-public interface ProductRepository extends JpaRepository<Product, UUID>, ProductRepositoryCustom {
+public interface ProductRepository {
+    Product save(Product product);
+
+    Optional<Product> findById(UUID id);
+
+    Slice<Product> searchByConditions(String name,
+                                      Integer minPrice,
+                                      Integer maxPrice,
+                                      UUID cursorId,
+                                      Pageable pageable);
+
+
+    List<Product> findAll();
+
+    void delete(Product product);
 }
